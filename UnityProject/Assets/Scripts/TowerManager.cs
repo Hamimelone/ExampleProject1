6233f7a1387f8ac75dd1df63fca681667d3b9dcd
风子Zugspitze <167789669+Hamimelone@users.noreply.github.com>
2025-07-16 14:49:48 +0200

    Delete UnityProject/Assets/Scripts/TowerManager.cs

--- UnityProject/Assets/Scripts/TowerManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TowerManager : Singleton<TowerManager>
-{
-    public List<Vector2>PlacedTowerList = new List<Vector2>();
-}
